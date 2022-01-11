7c6a82b2b5f432e8e7d6e759edf95a58433eb1ff
Simao Ferreira <simao.ferreira@zooplus.com>
2022-01-11 11:21:32 +0100

    NVIM: Autoinstall lsp servers, upadte configurations

--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -3,35 +3,54 @@ if not status_ok then
 	return
 end
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
+-- Include the servers you want to have installed by default below
+local servers = {
+  "bashls",
+  "cmake",
+  "dockerls",
+  "groovyls",
+  "jsonls",
+  "pyright",
+  "yamlls",
+  "sumneko_lua",
+  "zk",
+}
+
+for _, name in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found then
+    if not server:is_installed() then
+      print("Installing " .. name)
+      server:install()
+    end
+  end
+end
+
 lsp_installer.on_server_ready(function(server)
 	local opts = {
 		on_attach = require("lsp.handlers").on_attach,
 		capabilities = require("lsp.handlers").capabilities,
 	}
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
-
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
+    if server.name == "jsonls" then
+        local lopts = require("lsp.settings.jsonls")
+        opts = vim.tbl_deep_extend("force", lopts, opts)
+    end
 
     if server.name == "pyright" then
-	 	local pyright_opts = require("lsp.settings.pyright")
-	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	 end
+        local lopts = require("lsp.settings.pyright")
+        opts = vim.tbl_deep_extend("force", lopts, opts)
+    end
 
-    if server.name == "zk" then
-	 	local zk_opts = require("lsp.settings.zk")
-	 	opts = vim.tbl_deep_extend("force", zk_opts, opts)
-	 end
+    if server.name == "sumneko_lua" then
+        local lopts = require("lsp.settings.sumneko_lua")
+        opts = vim.tbl_deep_extend("force", lopts, opts)
+    end
 
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    if server.name == "yamlls" then
+        local lopts = require("lsp.settings.yamlls")
+        opts = vim.tbl_deep_extend("force", lopts, opts)
+    end
+
 	server:setup(opts)
 end)
