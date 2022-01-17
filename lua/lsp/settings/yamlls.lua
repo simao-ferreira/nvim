ebbb2e15e4c2a6943f2812ec75a30259d4e49f91
Simao Ferreira <simao.ferreira@zooplus.com>
2022-01-17 08:12:18 +0100

    NVIM: yaml config update

--- lua/lsp/settings/yamlls.lua
+++ lua/lsp/settings/yamlls.lua
@@ -5,5 +5,17 @@ return {
                 enabled = false
             },
         },
+        yaml = {
+            -- Schemas https://www.schemastore.org
+            schemas = {
+                ['http://json.schemastore.org/gitlab-ci.json'] = { '.gitlab-ci.yml' },
+                ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = {
+                    'docker-compose*.{yml,yaml}',
+                },
+                ['http://json.schemastore.org/github-workflow.json'] = '.github/workflows/*.{yml,yaml}',
+                ['http://json.schemastore.org/github-action.json'] = '.github/action.{yml,yaml}',
+                ['http://json.schemastore.org/prettierrc.json'] = '.prettierrc.{yml,yaml}',
+            },
+        },
     },
 }
