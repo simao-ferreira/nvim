4ebf467449edf86392fff564f91bdbb9e50044c9
Simao Ferreira <simao.ferreira@zooplus.com>
2022-01-18 14:52:16 +0100

    NVIM: Instructions for spell

--- lua/settings.lua
+++ lua/settings.lua
@@ -33,11 +33,23 @@ o.smartindent = true		-- indent a new line - extra stuff
 o.fileencoding = 'utf-8'
 
 -- Spell
--- to fix error: Cannot find word list
+
+-- to fix error: Cannot find word list.
 -- run `nvim -u NORC`
+-- :set spell
 -- :set spelllang=de
 -- :set spelllang=pt_pt
 -- It will prompt for the download of the lang files
+
+-- if it cannot download, then manually we can add the files.
+-- cd ~/.config/nvim/spell
+-- wget http://ftp.vim.org/vim/runtime/spell/pt.utf-8.spl
+
+-- shortcuts
+-- zg to add word to word list
+-- zw to reverse
+-- zug to remove word from word list
+-- z= to get list of possibilities
 o.spell = true                                  -- always spellcheck
 o.spelllang = { 'en', 'pt_pt', 'de_de'}         -- set languages for spellcheck
 
