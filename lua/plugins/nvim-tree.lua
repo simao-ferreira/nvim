1eb7ef3dc10ce685dbf13a049ab6bfea74a82e9a
Simao Ferreira <simao.ferreira@zooplus.com>
2022-06-28 20:42:23 +0200

    FIX | NVIM: More nvimtree updates, configurations are changing

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,74 +1,72 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+    return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-  return
+    return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = true,
-  ignore_ft_on_setup = {""},
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  view = {
-    width = 30,
-    side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-    number = false,
-    relativenumber = false,
-  },
-  renderer = {
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = true,
+    ignore_ft_on_setup = { "" },
+    open_on_tab = false,
+    hijack_cursor = false,
+    update_cwd = true,
+    -- update_to_buf_dir = {
+    --   enable = true,
+    --   auto_open = true,
+    -- },
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
         },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-            symlink = "",
+    },
+    view = {
+        width = 30,
+        side = "left",
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+                { key = "h", cb = tree_cb "close_node" },
+                { key = "v", cb = tree_cb "vsplit" },
+            },
         },
-      }
+        number = false,
+        relativenumber = false,
+    },
+    renderer = {
+        icons = {
+            glyphs = {
+                default = "",
+                symlink = "",
+                git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    deleted = "",
+                    untracked = "U",
+                    ignored = "◌",
+                },
+                folder = {
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                },
+            }
+        }
     }
-  }
 }
-
-
